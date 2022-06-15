dc2fd212a4cd51a3eca9808aec429c9506055a3a
so-heee <so.heee.mochi@gmail.com>
2022-06-15 09:59:55 +0900

    refactoring

--- api/infrastructure/router.go
+++ api/infrastructure/router.go
@@ -29,15 +29,6 @@ func Run() {
 		log.Fatal(err)
 	}
 
-	// jwtConfig := middleware.JWTConfig{
-	// 	Claims:     &JWTCustomClaims{},
-	// 	SigningKey: []byte("SECRET_KEY"),
-	// }
-	// g.Use(middleware.JWTWithConfig(jwtConfig))
-	// g.Use(middleware.JWT([]byte("secret")))
-
-	// g.Use(oapimiddleware.OapiRequestValidator(swagger))
-
 	g.Use(oapimiddleware.OapiRequestValidatorWithOptions(swagger, security.VerifyToken()))
 
 	conf := NewSQLConfig("user", "password", "db", "sample", 3306)
@@ -47,7 +38,6 @@ func Run() {
 	}
 
 	e.GET("/", healthCheck)
-	// e.POST("/login", login)
 	oapi.RegisterHandlers(g, controllers.NewController(h))
 
 	// Start server
@@ -57,30 +47,3 @@ func Run() {
 func healthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "healthy")
 }
-
-// func login(c echo.Context) error {
-// 	username := c.FormValue("username")
-// 	password := c.FormValue("password")
-//
-// 	// とりあえずのパスワード認証
-// 	if username != "taro" || password != "shhh!" {
-// 		return echo.ErrUnauthorized
-// 	}
-//
-// 	// トークン作成
-// 	token := jwt.New(jwt.SigningMethodHS256)
-//
-// 	claims := token.Claims.(jwt.MapClaims)
-// 	claims["name"] = "Taro"
-// 	claims["admin"] = true
-// 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-//
-// 	t, err := token.SignedString([]byte("secret"))
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	return c.JSON(http.StatusOK, map[string]string{
-// 		"token": t,
-// 	})
-// }
